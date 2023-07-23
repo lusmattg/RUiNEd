b80949a568493953e161fcd5ca8f1e2c909cdc90
lusmattg <lusmattg@users.noreply.github.com>
2023-07-23 09:05:32 -0400

    party can move from room to room

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ReactElement, useEffect, useState } from "react"
 import reactLogo from "./assets/rune.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
-import { GameState } from "./logic.ts"
+import { GameState, Room } from "./logic.ts"
 
 function App() {
   const [game, setGame] = useState<GameState>()
@@ -38,13 +38,57 @@ function App() {
     )
   })
 
+  if (game.choiceState == 'inAction') {
+    return (
+      <div>
+        <div>
+        </div>
+        <div>
+          <div>{game.currentRoom.name}</div>
+          <div>{game.currentRoom.desc}</div>    
+          <div>{game.choiceTimer}</div>
+          <button onClick={() => Rune.actions.skipToNextRoom({game: game})}>
+            vote
+        </button>
+    
+        </div>
+      </div>
+    )
+  }
+
+  const nextRoomChoices: Array<ReactElement> = [];
+  Object.keys(game.currentRoom.paths).forEach( (nextRoomId: string) => {
+    nextRoomChoices.push(
+      <button onClick={() => Rune.actions.changeRoom({game: game, newRoom: game.currentRoom.paths[nextRoomId]})}>
+        {game.currentRoom.paths[nextRoomId]}
+      </button>
+    )
+  });
+  if (game.choiceState == 'inVoteNext') {
+    return (
+      <div>
+        <div>
+        </div>
+        <div>
+          <div>{game.currentRoom.name}</div>
+          <div>{game.currentRoom.desc}</div>  
+          <div>
+            {nextRoomChoices}
+          </div>      
+        </div>
+      </div>
+    )    
+  }
+
   return (
     <>
-      <h1>Vite + Rune</h1>
       <div className="card">
         <button onClick={() => Rune.actions.increment({ amount: 1 })}>
           count is {game.count}
         </button>
+      </div>
+      <div>
+        
       </div>
       {playerList}
     </>
--- src/logic.ts
+++ src/logic.ts
@@ -2,8 +2,10 @@ import type { RuneClient } from "rune-games-sdk/multiplayer"
 
 export interface GameState {
   count: number,
-  currentSite: object,
+  currentRoom: Room,
   party: { [key: string]: PartyMember },
+  choiceState: string,
+  choiceTimer: number,
 }
 
 type GameActions = {
@@ -15,7 +17,65 @@ export type Party = {
 }
 
 export type PartyMember = {
-  hp: number,
+  maxHp: number,
+  curHp: number,
+  physAtk: number,
+  magAtk: number,
+  physDef: number,
+  magDef: number,
+}
+
+export type Enemy = {
+  maxHp: number,
+  curHp: number,
+  physAtk: number,
+  magAtk: number,
+  physDef: number,
+  magDef: number,
+}
+
+export type Treasure = {
+  name: string
+}
+
+export type RoomRune = {
+  name: string
+}
+
+export type Room = {
+  name: string,
+  intro: string,
+  desc: string,
+  sType: string,
+  sEnemies: Array<Enemy>,
+  sTreasure: Array<Treasure>,
+  sLocked: boolean,
+  sRune: RoomRune,
+  paths: Array<string>
+}
+
+export type Effect = {
+  name: string,
+  hpBuff: number,
+  duration: number,
+  effectType: string,
+  damageMult: number,
+  physAtkBuffSelf: number,
+  physAtkBuffParty: number,
+  physAtkNerfEnemy: number,
+  physAtkNerfEnemyParty: number,
+  magAtkBuff: number,
+  magAtkBuffParty: number,
+  magAtkNerfEnemy: number,
+  magAtkNerfEnemyParty: number,
+  physDefBuff: number,
+  physDefBuffParty: number,
+  physDefNerfEnemy: number,
+  physDefNerfEnemyParty: number,
+  magDefBuff: number,
+  magDefBuffParty: number,
+  magDefNerfEnemy: number,
+  magDefNerfEnemyParty: number,
 }
 
 declare global {
@@ -26,27 +86,124 @@ export function getCount(game: GameState) {
   return game.count;
 }
 
+
+
+const enemies = {
+  'eWhiteGem': {
+    name: 'White Gem',
+    maxHp: 10,
+    curHp: 10,
+    physAtk: 5,
+    magAtk: 5,
+    physDef: 5,
+    magDef: 5,
+  }
+}
+
+const rooms = {
+  'rTutorialRuneRoom': {
+    name: 'Tutorial - Runes',
+    intro: 'Mysterious etchings ahead',
+    desc: 'In RUiNEd, you\'ll encounter runes! This one makes you stronger!',
+    sType: 'rune',
+    sEnemies: [],
+    sTreasure: [],
+    sLocked: false,
+    sRune: null,
+    paths: ['rTutorialChoiceRoom']    
+  },
+  'rTutorialRestorationRoom':{
+    name: 'Tutorial - Restoration',
+    intro: 'The sound of water ahead',
+    desc: 'In RUiNEd, you\'ll encounter fountains! They restore your health.',
+    sType: 'restoration',
+    sEnemies: [],
+    sTreasure: [],
+    sLocked: false,
+    sRune: null,
+    paths: ['rTutorialRuneRoom']    
+  },
+  'rTutorialEnemyRoom':{
+    name: 'Tutorial - Enemies',
+    intro: 'Is that danger ahead?',
+    desc: 'In RUiNEd, you\'ll encounter monsters! In this fight, try taking out the White Gem first!',
+    sType: 'battle',
+    sEnemies: [],
+    sTreasure: [],
+    sLocked: false,
+    sRune: null,
+    paths: ['rTutorialRestorationRoom']  
+  },
+  'rTutorialTreasureRoom':{
+    name: 'Tutorial - Treasure',
+    intro: 'na',
+    desc: 'In RUiNEd, you\'ll find treasure! Try it now!',
+    sType: 'treasure',
+    sEnemies: [],
+    sTreasure: [],
+    sLocked: false,
+    sRune: null,
+    paths: ['rTutorialEnemyRoom']
+  },
+  'rTutorialChoiceRoom':{
+    name: 'Tutorial - Choice',
+    intro: 'The path forks ahead',
+    desc: 'In RUiNEd, you\'ll sometimes have to choose which room to visit next!',
+    sType: 'plain',
+    sEnemies: [],
+    sTreasure: [],
+    sLocked: false,
+    sRune: null,
+    paths: []  
+  }
+}
+
+
 Rune.initLogic({
   minPlayers: 1,
   maxPlayers: 4,
   setup: (): GameState => {
     return { 
       count: 0,
-      currentSite: {} ,
+      currentRoom: rooms['rTutorialTreasureRoom'],
       party: {},
+      choiceState: 'inAction',
+      choiceTimer: 60
+    }
+  },
+  update: ({game}) => {
+    //console.log(Rune.gameTimeInSeconds());
+    if (game.choiceTimer > -1) {
+      game.choiceTimer -= 1;
     }
   },
   actions: {
     increment: ({ amount }, { game }) => {
       game.count += amount
     },
+    voteNextRoom: ({vote}, {game}) => {
+      console.log('TODO');
+    },
+    changeRoom: ({newRoom},{game}) => {
+      console.log('CHANGING ROOM TO ' + newRoom);
+      game.currentRoom = rooms[newRoom];
+    },
+    skipToNextRoom: ({},{game}) => {
+      game.choiceTimer = 10;
+      game.choiceState = 'inVoteNext';
+    }
   },
   events: {
     playerJoined: (playerId: string, {game}) => {
-      console.log('player joined')
+      console.log('player joined', game)
+      console.log(Rune.actions)
       game.party[playerId] = {
-        hp: 40,
-
+        maxHp: 40,
+        curHp: 40,
+        physAtk: 10,
+        magAtk: 10,
+        physDef: 10,
+        magDef: 10,
       }
     },
     playerLeft(playerId: string, {game}) {
